f803b6d52e4b0714ad063c3942d48e066aba1662
wanyu <728372836@qq.com>
2017-04-06 18:35:00 +0800

    4_6_3

---
+++ app/src/main/java/com/doctor/yuyi/activity/RongConversation_Activity.java
@@ -0,0 +1,15 @@
+package com.doctor.yuyi.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.doctor.yuyi.R;
+
+public class RongConversation_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rong_conversation_);
+    }
+}
